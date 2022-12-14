ee4d2a8b73e23ecd67092514e1be49092f2d92ad
kelvinmasuyama@gmail.com <kelvinmasuyama@gmail.com>
2022-12-14 00:59:13 +0000

    Aprendendo WHERE AND IN

---
+++ WHERE.sql
@@ -0,0 +1,53 @@
+-- Databricks notebook source
+SELECT * 
+FROM silver_olist.pedido
+
+where descSituacao = 'delivered'
+
+LIMIT 100
+
+-- leia-se: selecione todas as colunas da tabela silver_olist.pedido onde a situação do pedido seja entregue 'delived', (caso queira que seja diferente coloque o sinal <>)
+
+-- COMMAND ----------
+
+SELECT * 
+FROM silver_olist.pedido
+
+where (descSituacao = 'shipped' OR descSituacao = 'canceled')
+AND YEAR(dtPedido) = '2018'
+
+-- leia-se: selecione todas as colunas da tabela silver_olist.pedido filtrando pedidos (enviados ou cancelados) de 2018
+
+LIMIT 1000
+
+
+
+-- COMMAND ----------
+
+SELECT * 
+FROM silver_olist.pedido
+
+where descSituacao IN ('shipped','canceled')
+AND YEAR(dtPedido) = '2018'
+
+LIMIT 1000
+
+-- leia-se: selecione todas as colunas da tabela silver_olist.pedido filtrando pedidos (enviados ou cancelados) de 2018
+
+-- COMMAND ----------
+
+SELECT *,
+       DATEDIFF(dtEstimativaEntrega, dtAprovado)
+       
+FROM silver_olist.pedido
+
+where descSituacao IN ('shipped','canceled')
+AND YEAR(dtPedido) = '2018'
+AND DATEDIFF(dtEstimativaEntrega, dtAprovado) > 30
+
+LIMIT 1000
+
+
+-- COMMAND ----------
+
+
